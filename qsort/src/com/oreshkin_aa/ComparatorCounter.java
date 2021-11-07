af3eb2655f851e13a3d6a7ef2368d9ca81c46c2f
artem98 <temiko_oreshkin@bk.ru>
2021-11-07 19:33:43 +0300

    NEW: cmp: add comparator-counter

---
+++ qsort/src/com/oreshkin_aa/ComparatorCounter.java
@@ -0,0 +1,38 @@
+package com.oreshkin_aa;
+
+import java.util.Comparator;
+
+public class ComparatorCounter<T extends Comparable<T>> implements java.util.Comparator<T> {
+
+    private int mComparesCount;
+    private boolean mIsDefaultCompare = true;
+    private Comparator<T> mCustomComparator;
+
+    public ComparatorCounter() {
+        mComparesCount = 0;
+    }
+
+    public ComparatorCounter(Comparator<T> comparator) {
+        mComparesCount = 0;
+        mCustomComparator = comparator;
+        mIsDefaultCompare = false;
+    }
+
+
+    public void clearCount() { mComparesCount = 0; }
+    public int getComparesCount() {
+        return mComparesCount;
+    }
+    public void setComparesCount(int mComparesCount) {
+        this.mComparesCount = mComparesCount;
+    }
+
+    @Override
+    public int compare(T o1, T o2) {
+        mComparesCount++;
+
+        if(mIsDefaultCompare)
+            return o1.compareTo(o2);
+        return mCustomComparator.compare(o1, o2);
+    }
+}
