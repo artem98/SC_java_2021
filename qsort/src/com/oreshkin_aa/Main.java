85ef474e4fae102dcfc7eb2612695decd860f264
artem98 <temiko_oreshkin@bk.ru>
2021-11-09 00:12:06 +0300

    NEW: print array func

--- qsort/src/com/oreshkin_aa/Main.java
+++ qsort/src/com/oreshkin_aa/Main.java
@@ -4,5 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+    public static <T> void printArr(T[] arr) {
+        System.out.print("[");
+        for(int i = 0; i < arr.length - 1; i++)
+            System.out.print(arr[i] + ", ");
+        System.out.println(arr[arr.length - 1] + "]");
     }
 }
+
