5db5492729cd67f53abd72bdf9f8932714f3a82d
artem98 <temiko_oreshkin@bk.ru>
2021-12-15 19:28:59 +0300

    CALC: new: evaluate list of stack operations

--- Calculator/src/com/oreshkin_aa/calc/StackCalculator.java
+++ Calculator/src/com/oreshkin_aa/calc/StackCalculator.java
@@ -2,18 +2,36 @@ package com.oreshkin_aa.calc;
 
 import com.oreshkin_aa.calc.tokens.Token;
 
+import java.util.EmptyStackException;
+import java.util.List;
 import java.util.Stack;
 
 public class StackCalculator {
 
-    private Stack<Token> tokenStack;
+    private List<Token> tokenList;
     private Stack<Double> stack;
 
-    public StackCalculator(Stack<Token> tokenStack) {
-        this.tokenStack = tokenStack;
+    public StackCalculator(List<Token> tokenList) {
+        this.tokenList = tokenList;
+        stack = new Stack<>();
     }
 
-    public double calculate() {
-
+    public double calculate() throws Exception {
+        for(Token token : tokenList) {
+            try {
+                token.applyToStack(stack);
+            }
+            catch (EmptyStackException e) {
+                throw new Exception("Wrong expression!");
+            }
+        }
+        double result;
+        try {
+            result = stack.pop();
+        }
+        catch (EmptyStackException e) {
+            throw new Exception("Wrong expression!");
+        }
+        return result;
     }
 }
