661f3a94b5649f5faf7d7aa03720a9ef152f9768
artem98 <temiko_oreshkin@bk.ru>
2021-12-15 19:39:21 +0300

    CALC: new: test calculator in main

--- Calculator/src/com/oreshkin_aa/calc/Main.java
+++ Calculator/src/com/oreshkin_aa/calc/Main.java
@@ -1,9 +1,30 @@
 package com.oreshkin_aa.calc;
 
+import com.oreshkin_aa.calc.tokens.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("s");
+
+        ArrayList<Token> list = new ArrayList<>();
+        list.add(new TokenNumber(2));
+        list.add(new TokenNumber(3));
+        list.add(new TokenUnaryMinus());
+
+        StackCalculator calculator = new StackCalculator(list);
+        double v = 0;
+        try {
+            v = calculator.calculate();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println("Result " + v);
     }
 }
