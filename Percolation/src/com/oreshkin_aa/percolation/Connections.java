e68e5ba062a4610b502faf8089a0a19eb00445c0
artem98 <temiko_oreshkin@bk.ru>
2021-12-24 23:12:54 +0300

    PERC: new: connections union and find realization

--- Percolation/src/com/oreshkin_aa/percolation/Connections.java
+++ Percolation/src/com/oreshkin_aa/percolation/Connections.java
@@ -1,25 +1,50 @@
 package com.oreshkin_aa.percolation;
 
-import java.util.function.BiConsumer;
-import java.util.function.Consumer;
-import java.util.function.Supplier;
-
 public class Connections {
 
     public int N;
-    public int[] components;
+    public int[] parent;
+    public int[] rank;
 
     public Connections(int N) {
         this.N = N;
-        components = new int[N];
+        parent = new int[N];
+        rank = new int[N];
+        clear();
+    }
+
+    public int find(int x) {
+        if(parent[x] == x)
+            return x;
+
+        int findRes = find(x);
+        parent[x] = findRes;
+        return findRes;
+    }
+
+    public void union(int x, int y) {
+        x = find(x);
+        y = find(y);
+        if(x == y)
+            return;
+
+        if(rank[x] < rank[y])
+            parent[x] = y;
+        else {
+            parent[y] = x;
+            if(rank[x] == rank[y])
+                rank[x]++;
+        }
     }
 
     public void clear() {
-        for(int i = 0; i < N; i++)
-            components[i] = i;
+        for(int i = 0; i < N; i++) {
+            parent[i] = i;
+            rank[i] = 0;
+        }
     }
 
     public int getComponent(int ind) {
-        return components[ind];
+        return parent[ind];
     }
 }
